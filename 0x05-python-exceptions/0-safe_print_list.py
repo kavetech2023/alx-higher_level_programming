4fc010705072d85b7457368cc0f28b23199ae8a7
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-24 15:39:04 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    sum_total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            sum_total += 1
-        except IndexError:
-            break
-    print()
-    return(sum_total)
+  """Prints the first x elements of a list, handling potential IndexError.
+
+  Args:
+    my_list: The list to print elements from.
+    x: The number of elements to print (defaults to all elements).
+
+  Returns:
+    The number of elements successfully printed.
+  """
+
+  sum_total = 0
+  for i in range(x):
+    try:
+      print(f"{my_list[i]}", end="")
+      sum_total += 1
+    except IndexError:
+      break
+  print()
+  return sum_total
